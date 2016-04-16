8bfc1772443e0b8a234e85d301cc493f0ffc2be6
Craggle <SPSallay@gmail.com>
2016-04-16 16:52:25 -0400

    Implements Display module

--- src/modules/display.js
+++ src/modules/display.js
@@ -1,6 +1,31 @@
+const CHANGE_TAB = 'CHANGE_TAB';
+const TOGGLE_MODAL = 'TOGGLE_MODAL';
+/*
+ * Display Reducer
+ *
+ * state has the following properties:
+ * @tab: The index of the currently visible view container
+ * @modal: A boolean defining modal visibility
+ *
+ */
 export default function reducer(state, action) {
   switch (action.type) {
+    case CHANGE_TAB:
+      return state.set('tab', action.tab);
+    case TOGGLE_MODAL:
+      return state.set('modal', !(state.get('modal')));
     default:
       return state;
   }
+}
+
+export function changeTab(tab) {
+  return {
+    type: CHANGE_TAB,
+    tab,
+  };
+}
+
+export function toggleModal() {
+  return { type: TOGGLE_MODAL };
 }
